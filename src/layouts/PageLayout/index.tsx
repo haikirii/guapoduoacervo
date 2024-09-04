bd561d178be2f1996c498f89301c295728381b63
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-04 15:09:54 -0300

    renomeia arquivos jsx para tsx

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -1,6 +1,6 @@
-import Header from "../../components/Header";
+import Header from "../../components/Header/";
 import { Outlet } from "react-router-dom";
-import Footer from "../../components/Footer";
+import Footer from "../../components/Footer/";
 
 const PageLayout = () =>{
     return(
