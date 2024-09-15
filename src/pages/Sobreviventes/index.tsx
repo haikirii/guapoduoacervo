f24cf109e288b6501400c954406d44ea95c6c044
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-14 23:27:16 -0300

    adiciona tela sobreviventes

--- src/pages/Sobreviventes/index.tsx
+++ src/pages/Sobreviventes/index.tsx
@@ -2,19 +2,23 @@ import { Helmet } from 'react-helmet';
 import styles from './index.module.scss';
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
 import { db } from '../../service/firebase-config';
-import { useEffect, useState } from "react";
-
+import { useContext, useEffect, useState } from "react";
+import { ThemeContext } from '../../contexts/ThemeContext';
+import guapoduo from '/image/guapoduo/plantando.png?url';
+import "../../styles/base/_reset.scss"
 
 function gerarNumeroAleatorio() {
-  var numero = '';
+  let numero = '';
 
-  var timestamp = new Date().getTime();
+  let timestamp = new Date().getTime();
   numero = timestamp.toString().substr(-4);
 
   return numero;
 }
 
 const Sobreviventes = () => {
+  
+  const { theme } = useContext(ThemeContext);
 
   const ticket = gerarNumeroAleatorio();
 
@@ -26,9 +30,25 @@ const Sobreviventes = () => {
   return (
     <>
       <Helmet> <title> Você Sobreviveu! </title> </Helmet>
-      <div className={styles.container}>
-        testando {ticket}
-      </div>
+      <main className={`${styles.main} ${styles[theme]} wrapper`}>
+          <div className={styles.containerImagem}>
+            <img src={guapoduo} className={styles.imagem} />
+          </div>
+          <div className={styles.containerTicket}>
+            <div className={styles.ticket}>
+              <div className={styles.titulo}>
+                  Você sobreviveu!
+              </div>
+              <div className={styles.ticketNumero}>
+                <span className={styles.txtTck}>Nº Ticket:</span>
+                <span className={styles.nTck}>{ticket}</span>
+              </div>
+              <div className={styles.textoAbaixo}>
+                Parabéns, você sobreviveu ao ARGuapoduo, esse é seu ticket de resgate, com ele você terá seu badge exclusivo em nossa plataforma no universo guapoduo! Seu prêmio te aguarda no site, não compartilhe suas informações com <span className={styles.bold}>NINGUÉM</span>.
+              </div>
+            </div>
+          </div>
+      </main>
     </>
   )
 }
