77609d7dc94469c73eaa5a7053a184fe8e5e0842
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-18 20:38:06 -0300

    troca home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageLayout from "../layouts/PageLayout";
 import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
-import Perpetuo from "../pages/Perpetuo";
-import Wip from "../pages/Wip";
-import SempreTeAmarei from "../pages/SempreTeAmarei";
-import Saudade from "../pages/Saudade";
-import Familia from "../pages/Familia";
-import Sobreviventes from "../pages/Sobreviventes";
-import TemNadaAqui from "../pages/TemNadaAqui";
+import Home from "../pages/Home";
 
 export const Paths = () => {
     return (
@@ -16,14 +10,8 @@ export const Paths = () => {
             <BrowserRouter basename={import.meta.env.BASE_URL}>
                 <Routes>
                     <Route path={'/'} element={<PageLayout />}>
-                        <Route index element={<Wip />} />
+                        <Route index element={<Home />} />
                         <Route path={'/login'} element={<Login />} />
-                        <Route path={'/perpetuo'} element={<Perpetuo />} />
-                        <Route path={'/sobreviventes'} element={<Sobreviventes/>} />
-                        <Route path={'/temnadaaqui'} element={<TemNadaAqui/>} />
-                        <Route path={'/sempreteamarei'} element={<SempreTeAmarei />} />
-                        <Route path={'/saudade'} element={<Saudade />} />
-                        <Route path={'/familia'} element={<Familia />} />
                         {/* <Route index element={<Home />}/>
                         <Route path={'/Guapoverso'} element={<Guapoverso />}/>
                         <Route path={'/Categoria'}element={<Categoria />}/>
