2da856bccaed12f2ae99c0037ef5e0a48f917679
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-20 18:59:42 -0300

    componentiza svgs footer & links footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,64 +6,79 @@ import engImg from '/image/components/languages/eng.png'
 import styles from "./index.module.scss";
 
 import BotaoMudarIdioma from '../BotaoMudarIdioma';
+import FooterLinks from '../FooterLinks';
+import { Item } from '../FooterLinks';
 
-const Footer = () =>{
+const svgIconProv = (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
+    </svg>
+)
+
+const Footer = () => {
 
     const { theme } = useContext(ThemeContext);
+
+    const Sitemap: Item[] = [
+        { href: "/t", text: "Link de Página Link de Página", isLink: true, isSvg: false },
+        { href: "/t", text: "Link de Página Link de Página", isLink: true, isSvg: false },
+        { href: "/t", text: "Link de Página Link de Página", isLink: true, isSvg: false },
+        { href: "/t", text: "Link de Página Link de Página", isLink: true, isSvg: false },
+    ];
+
+    const Suporte: Item[] = [
+        { href: "/t", text: "FAQ", isLink: true, isSvg: false },
+        { href: "/t", text: "Como Funcionamos", isLink: true, isSvg: false },
+    ];
+
+    const Sobre: Item[] = [
+        { href: "/t", text: "Administradores", isLink: true, isSvg: false },
+        { href: "/t", text: "Regras", isLink: true, isSvg: false },
+        { href: "/t", text: "Formulário", isLink: true, isSvg: false },
+    ];
+
+    const Contato: Item[] = [
+        { href: "/t", text: "", isLink: true, isSvg: true, svgIcon: svgIconProv },
+        { href: "/t", text: "", isLink: true, isSvg: true, svgIcon: svgIconProv },
+        { href: "/t", text: "", isLink: true, isSvg: true, svgIcon: svgIconProv },
+        { href: "/t", text: "", isLink: true, isSvg: true, svgIcon: svgIconProv },
+        { text: "email@exemplo.com", isLink: false, isSvg: false },
+    ];
 
     return(
         <footer className={`${styles.footer} ${styles[theme]}`}>
             <div className={`${styles.container} wrapper`}>
-                <div className={`${styles.siteMap}`}>
-                    <h4>SiteMap</h4>
-                    <a href="/t">Link de Página</a>
-                    <a href="/t">Link de Página</a>
-                    <a href="/t">Link de Página</a>
+                <div className={``}>
+                    <FooterLinks
+                        title='Sitemap'
+                        items={Sitemap}
+                    />
                 </div>
-                <div className={`${styles.suporte}`}>
-                    <h4>Suporte</h4>
-                    <a href="/t">FAQ</a>
-                    <a href="/t">Link de Página</a>
+                <div className={``}>
+                    <FooterLinks
+                        title='Suporte'
+                        items={Suporte}
+                    />
                 </div>
-                <div className={`${styles.sobre}`}>
-                    <h4>Sobre</h4>
-                    <a href="/t">Administradores</a>
-                    <a href="/t">Regras</a>
-                    <a href="/t">Formulário</a>
+                <div className={``}>
+                    <FooterLinks
+                        title='Sobre'
+                        items={Sobre}
+                    />
                 </div>
-                <div className={`${styles.contato}`}>
-                    <h4>Contato</h4>
-                    <div className={`${styles.contatoSvg}`}>
-                        <a href="/t">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                            </svg>
-                        </a>
-                        <a href="/t">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                            </svg>
-                        </a>
-                        <a href="/t">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                            </svg>
-                        </a>
-                        <a href="/t">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                            </svg>
-                        </a>
-                    </div>
-                    <span>email@exemplo.com</span>
+                <div className={``}>
+                    <FooterLinks
+                        title='Contato'
+                        items={Contato}
+                    />
                 </div>
             </div>
             <div className={`${styles.lang} wrapper`}>
                 <BotaoMudarIdioma src={brImg} lang={'portugês'} />
                 
-                <BotaoMudarIdioma src={espImg} lang={'portugês'} />
+                <BotaoMudarIdioma src={espImg} lang={'espanhol'} />
                 
-                <BotaoMudarIdioma src={engImg} lang={'portugês'} />
+                <BotaoMudarIdioma src={engImg} lang={'inglês'} />
             </div>
             <div className={`${styles.comerc} wrapper`}>
                 <span>Acervo Amarando © 2024</span>
