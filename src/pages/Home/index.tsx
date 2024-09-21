e8f9d8c053a7215f319149b4f947864c106dceca
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-21 19:58:04 -0300

    limpa home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,10 @@
 import { Helmet } from 'react-helmet';
-import Plantando from '../../components/Plantando';
-const Wip = () => {
 
-  const data = {
-    title: "Nada por aqui",
-    texto: "Ainda estamos plantando as sementes de amaranto..."
-  }
+const Wip = () => {
 
   return (
     <>
       <Helmet> <title>Plantando Amarantos</title> </Helmet>
-        <Plantando 
-          title={data.title} 
-          texto={data.texto}
-          botao={true}
-        />
     </>
   )
 }
