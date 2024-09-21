1309539a6196d378775eef0969c563a1c8afd9ce
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-21 20:38:00 -0300

    coloca banner na tela com texto

--- src/components/BannerHome/index.tsx
+++ src/components/BannerHome/index.tsx
@@ -1,10 +1,20 @@
 import styles from './index.module.scss';
+import banner from '/temp/Banner.png';
 
 const BannerHome = () => {
   return (
-    <>
-    
-    </>
+    <div className={`${styles.banner}`}>
+      <div className={`${styles.bannerContent} wrapper`}>
+        <h1>
+          Onde histórias são feitas para serem eternizadas
+        </h1>
+        <p>
+          Arquivo exclusivo de universos protagonizados pelos Guapoduo!
+        </p>
+        {/* adicionar alt após colocar o banner finalizado */}
+        {/* <img src={banner} alt="" /> */}
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Helmet } from 'react-helmet';
+import BannerHome from '../../components/BannerHome';
 
 const Home = () => {
 
   return (
     <>
       <Helmet> <title>Plantando Amarantos</title> </Helmet>
+      <BannerHome />
     </>
   )
 }
