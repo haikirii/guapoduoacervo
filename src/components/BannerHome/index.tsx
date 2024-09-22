f7693580b1d7e5e371a5ea8dc793e65914239c8c
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-21 21:14:23 -0300

    adiciona banner completo

--- src/components/BannerHome/index.tsx
+++ src/components/BannerHome/index.tsx
@@ -1,18 +1,22 @@
 import styles from './index.module.scss';
-import banner from '/temp/Banner.png';
+import guapoduoBanner from '/temp/guapoduo-banner-temp.png';
 
 const BannerHome = () => {
   return (
     <div className={`${styles.banner}`}>
-      <div className={`${styles.bannerContent} wrapper`}>
-        <h1>
-          Onde histórias são feitas para serem eternizadas
-        </h1>
-        <p>
-          Arquivo exclusivo de universos protagonizados pelos Guapoduo!
-        </p>
+      <div className={`${styles.bannerContent}`}>
+        <div className={styles.textoBanner}>
+          <h1 className={styles.tituloPage}>
+            Onde histórias são feitas para serem eternizadas
+          </h1>
+          <p>
+            Arquivo exclusivo de universos protagonizados pelos Guapoduo!
+          </p>
+        </div>
         {/* adicionar alt após colocar o banner finalizado */}
-        {/* <img src={banner} alt="" /> */}
+        <div className={styles.imgBanner}>
+          <img src={guapoduoBanner} alt="" />
+        </div>
       </div>
     </div>
   )
