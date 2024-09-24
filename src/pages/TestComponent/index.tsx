1961e8a1c729878661a3b08325ac53b42c4e5cdc
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-24 14:39:36 -0300

    adiciona card na pagina de teste

--- src/pages/TestComponent/index.tsx
+++ src/pages/TestComponent/index.tsx
@@ -1,9 +1,35 @@
 import styles from './index.module.scss';
 
+import CardFanart from '../../components/HomeComponents/CardFanart/index.';
+
+import profilePic from '/image/about/Ren-pfp.png';
+import fanart1 from '/temp/fanart-ren1.png';
+import fanart2 from '/temp/fanart-ren2.png';
+
+
+const cardFanartInfo1 = {
+  fanart: fanart1,
+  title: 'Título fanart 1',
+  profilePic: profilePic,
+  profileName: 'Ren',
+}
+
+const cardFanartInfo2 = {
+  fanart: fanart2,
+  title: 'Título fanart 2',
+  profilePic: profilePic,
+  profileName: 'Ren',
+}
+
 const TestComponent = () => {
   return (
     <>
-      a
+      <CardFanart 
+          fanart={cardFanartInfo1.fanart}
+          title={cardFanartInfo1.title}
+          profilePic={cardFanartInfo1.profilePic}
+          profileName={cardFanartInfo1.profileName}
+        />
     </>
   )
 }
