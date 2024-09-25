27569f803fe3a28722b83852726d54b05cfc53ed
CodeByGab <gabrielsoares1602@gmail.com>
2024-09-25 16:14:20 -0300

    remove teste da pasta

--- src/pages/TestComponent/index.tsx
+++ src/pages/TestComponent/index.tsx
@@ -1,16 +1,10 @@
-import AtualizacaoListagem from '../../components/CardAtualizacao';
 import styles from './index.module.scss';
 
 const TestComponent = () => {
   return (
     <>
       <div className={styles.fundo}>
-        <AtualizacaoListagem tipoAtualizacao='Nova capítulo!'/>
-        <AtualizacaoListagem tipoAtualizacao='Nova capítulo!'/>
-        <AtualizacaoListagem tipoAtualizacao='Nova capítulo!'/>
-        <AtualizacaoListagem tipoAtualizacao='Nova capítulo!'/>
-        <AtualizacaoListagem tipoAtualizacao='Nova capítulo!'/>
-        <AtualizacaoListagem tipoAtualizacao='Nova capítulo!'/>
+
       </div>
     </>
   )
