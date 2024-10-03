f4e62b545cd6f0aa7ae8286a3fb783fd1adc81f3
Liazinea <125595851+liazinea@users.noreply.github.com>
2024-10-02 22:49:42 -0300

    Arruma tag TituloAtualizacao

--- src/components/CardAtualizacao/TituloAtualizacao/index.tsx
+++ src/components/CardAtualizacao/TituloAtualizacao/index.tsx
@@ -6,7 +6,7 @@ interface TituloProps {
 
 const TituloAtualizacao = ({texto}: TituloProps) => {
     return  (
-        <h3 className={styles.titulo}>{texto}</h3>
+        <h4 className={styles.titulo}>{texto}</h4>
     )
     ;
 }
