75b723952559f5f29a3584ff35d41b1a841c4a6d
Liazinea <125595851+liazinea@users.noreply.github.com>
2024-11-04 21:08:38 -0300

    Botao texto (#14)
    * Adiciona components botao-texto
    
    * Arruma BotaoDropdown e links
    
    * Adiciona as logos e style para icons
    
    ---------
    
    Co-authored-by: KellerBeMaciel <bkelly.sm@gmail.com>

--- src/pages/TestComponent/index.tsx
+++ src/pages/TestComponent/index.tsx
@@ -1,4 +1,5 @@
-import '../../styles/icons/style.css';
+
+import BotaoTexto from '../../components/BotaoTexto';
 import styles from './index.module.scss';
 import InteracaoBotaoFanfic from '../../components/InteracaoBotaoFanfic';
 import AtualizacaoListagem from '../../components/CardAtualizacao';
@@ -10,28 +11,7 @@ import '../../styles/icons/style.css';
 const TestComponent = () => {
   return (
     <div className={styles.fundo}>
-     <InteracaoBotaoFanfic/>
-     
-     <AtualizacaoListagem 
-      pathCapa={Capa} 
-      titulo={'The reason'} 
-      novoCap={false} 
-      textoBotao={'Ler Agora'}
-      linkFanfic={'asaasd'}
-     />
-
-     <CardFanfic 
-     pathCapa={Capa}
-     titulo={'The reason'}
-     pathClassificacao={Classificacao}
-     altClassificação={'Recomendado para maiores de 16 anos'}
-     autore={'Nan_yelo'}
-     sinopse={"Vazio. Tédio. Insatisfação. Isso era tudo que ele sentia, e só precisava de uma distração... O que ele não esperava, no entanto, é que essa distração viria na forma de um lindo mexicano que estava prestes a endireitar sua vida torta para sempre."}
-     comentario={138}
-     capitulo={'19/?'} 
-     data={'aug. 2023'}
-     linkFanfic={'saasas'} 
-     linkAutore={'vdvveef'} />
+      <BotaoTexto/>
     </div>
   )
 }
---
+++ src/types/BotaoTextoType.ts
@@ -0,0 +1,6 @@
+export interface BotaoTextoProps {
+    texto: string;
+    icon: string;
+    link?: string;
+    open?: boolean;
+  }
