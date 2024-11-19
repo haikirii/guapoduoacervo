64dd2cfdd23685620c8e9707485b2121679e5969
CodeByGab <gabrielsoares1602@gmail.com>
2024-11-19 18:47:24 -0300

    adiciona botão acessar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../../contexts/AuthContext";
 import HeaderHomeLink from "./HeaderHomeLink";
 import HeaderLinks from "./HeaderLinks";
 import HeaderSearch from "./HeaderSearch";
+import HeaderAcessar from "./HeaderAcessar";
 
 interface changeSearchOpen {
   changeOpen: () => void;
@@ -21,19 +22,25 @@ const Header = () => {
   const { isLogged, setIsLogged } = authContext;
 
   function changeOpen() {
-    setIsSearchOpen(!isSearchOpen)
-  } 
+    setIsSearchOpen(!isSearchOpen);
+  }
 
   return (
     <header className={`${styles.header} ${styles[theme]}`}>
       <div className={`${styles.headerContent} wrapper`}>
         <HeaderHomeLink />
-        <HeaderLinks nomePagina="Descubra" link="/t" compOpen={isSearchOpen}/>
-        <HeaderLinks nomePagina="Fanfics" link="/t" compOpen={isSearchOpen}/>
-        <HeaderLinks nomePagina="Fanarts" link="/t" compOpen={isSearchOpen}/>
-        {!isLogged && <HeaderLinks nomePagina="Sobre" link="/t" compOpen={isSearchOpen}/>}
+        <HeaderLinks nomePagina="Descubra" link="/t" compOpen={isSearchOpen} />
+        <HeaderLinks nomePagina="Fanfics" link="/t" compOpen={isSearchOpen} />
+        <HeaderLinks nomePagina="Fanarts" link="/t" compOpen={isSearchOpen} />
+        {!isLogged && (
+          <HeaderLinks nomePagina="Sobre" link="/t" compOpen={isSearchOpen} />
+        )}
         <HeaderSearch changeOpen={changeOpen} isOpen={isSearchOpen} />
-        {isLogged ? <h1>logado</h1> : <h1>deslogado</h1>}
+        {isLogged ? 
+          <h1>logado</h1> 
+          :
+          <HeaderAcessar />
+        }
       </div>
     </header>
   );
