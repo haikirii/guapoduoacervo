22d25bb644d7216379f101255ce217a7ad1a4b17
Liazinea <feitosalivia23@gmail.com>
2024-11-25 17:41:22 -0300

    Adiciona estilização CapituloInfo

--- src/components/CapituloInfo/index.tsx
+++ src/components/CapituloInfo/index.tsx
@@ -1,15 +1,22 @@
 import styles from './index.module.scss';
 import { CapituloInfoProps } from '../../types/CapituloInfoType';
+import '../../styles/icon/style.css';
+import InfoFanfic from '../CardFanfic/InfoFanfic';
 
 const CapituloInfo = (props: CapituloInfoProps) => {
+  let aux = '';
+  if(props.numeroCap < 10){
+    aux = '0';
+  }
   return (
     <div className={styles.principal}>
-      <p className={styles.numero}>{props.numero}</p>
-      <p className={styles.titulo}>{props.titulo}</p>
-      <p className={styles.data}>{props.data}</p>
-      <div className={styles.cutidas}>
-        <i className={props.icon}/>
-        <p className={styles.quantidade}>{props.curtida}</p>
+      <div className={styles.texto}>
+        <p className={styles.numero}>#{`${aux}${props.numeroCap}`}</p>
+        <p className={styles.titulo}>{props.titulo}</p>
+      </div>
+      <div className={styles.tamanho}>
+        <InfoFanfic icon={'ol-history'} infoString={props.data} />
+        <InfoFanfic icon={'ol-heart'} infoNumber={256}/>
       </div>
     </div>
   )
