8e4a67b625cd0094fcfabd00ea210c9f4d3299ee
CodeByGab <gabrielsoares1602@gmail.com>
2024-11-25 15:20:56 -0300

    remove arquivos teste

--- src/pages/TestComponent/index.tsx
+++ src/pages/TestComponent/index.tsx
@@ -1,50 +1,13 @@
-import Creditos from '../../components/Creditos';
-import styles from './index.module.scss';
-import DescubraHome from '../../components/DescubraHome';
+import Creditos from "../../components/Creditos";
+import styles from "./index.module.scss";
+import DescubraHome from "../../components/DescubraHome";
 
-import profPic from '/image/about/Ren-pfp.png'
-import fanart from '/temp/fanart-ren1.png'
-import cover from '/temp/capa.jpg'
-
-import CardFanart from '../../components/HomeComponents/CardFanart/index.';
-
-import profilePic from '/image/about/Ren-pfp.png';
-import fanart1 from '/temp/fanart-ren1.png';
-import fanart2 from '/temp/fanart-ren2.png';
-
-
-const cardFanartInfo1 = {
-  fanart: fanart1,
-  title: 'Título fanart 1',
-  profilePic: profilePic,
-  profileName: 'Ren',
-}
-
-const cardFanartInfo2 = {
-  fanart: fanart2,
-  title: 'Título fanart 2',
-  profilePic: profilePic,
-  profileName: 'Ren',
-}
+import profPic from "/image/about/Ren-pfp.png";
+import fanart from "/temp/fanart-ren1.png";
+import cover from "/temp/capa.jpg";
 
 const TestComponent = () => {
-  return (
-    <>
-      <CardFanart 
-          fanart={cardFanartInfo1.fanart}
-          title={cardFanartInfo1.title}
-          profilePic={cardFanartInfo1.profilePic}
-          profileName={cardFanartInfo1.profileName}
-        />
-        
-      <CardFanart 
-          fanart={cardFanartInfo2.fanart}
-          title={cardFanartInfo2.title}
-          profilePic={cardFanartInfo2.profilePic}
-          profileName={cardFanartInfo2.profileName}
-        />
-    </>
-  )
-}
+  return <></>;
+};
 
 export default TestComponent;
