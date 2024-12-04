93d323fc77584a7793e55cd9588c30c55e78e7c0
CodeByGab <gabrielsoares1602@gmail.com>
2024-12-03 21:16:43 -0300

    muda fonte footer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import { Helmet } from 'react-helmet';
 import BannerHome from '../../components/HomeComponents/BannerHome';
+import Plantando from '../../components/Plantando';
 
 const Home = () => {
 
   return (
     <>
       <Helmet> <title>Plantando Amarantos</title> </Helmet>
-      <BannerHome />
+      {/* <BannerHome /> */}
+      <Plantando title={'Nada por aqui'} texto={'Ainda estamos plantando as sementes de amaranto...'} />
     </>
   )
 }
